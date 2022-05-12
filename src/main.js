d129c342dcaa074ba61b1d84452e4023d92694c8
Guan-Ting Liu <liugoldent@gmail.com>
2022-05-12 21:29:50 +0800

    vue3 初步v-for

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ app.config.globalProperties.envURL =
     process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8080' : 'https://news-backend-g.herokuapp.com';
 
 
-axios.defaults.timeout = 5000;
+axios.defaults.timeout = 50000;
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'; // 定義內容類型為application....
 axios.defaults.baseURL = '/api' // 打api的基本URL，打任何網址都會加上這個
 
--- src/views/SelfView.vue
+++ src/views/SelfView.vue
@@ -3,51 +3,108 @@
     <h1>This is an self page</h1>
     <button @click="get">Get Func</button>
     <button @click="post">Post Func</button>
-    <h1>{{ count }}</h1>
+    <ol>
+      <li v-for="(data,index) in buy_page_list">
+        {{ data.name }}
+      </li>
+    </ol>
+    <ol>
+      <li v-for="(data,index) in sell_page_list">
+        {{ data.name }}
+      </li>
+    </ol>
   </div>
 </template>
 <script>
-import { ref, getCurrentInstance, onMounted } from 'vue'
+import { ref, getCurrentInstance, onMounted, reactive } from "vue";
 export default {
-    setup(){
-        const count = ref(0);
-        const increment = () => count.value++;
+  setup() {
+    // 設定API
+    const { proxy } = getCurrentInstance();
+    const getInvestLocal = proxy.axios.get(
+      `${proxy.envURL}/stockApi/sheetData/invest/local_listed`
+    );
+    // const tech_avg_url = `${proxy.envURL}/stockApi/sheetData/tech/avg`
+    // const tech_local_url = `${proxy.envURL}/stockApi/sheetData/tech/local_buy_sell`
+    // const tech_foreign_url = `${proxy.envURL}/stockApi/sheetData/tech/foreign_buy_sell`
+    const kd_promise = proxy.axios.get(
+      `${proxy.envURL}/stockApi/sheetData/tech/kd`
+    );
+    // const macd_promise = proxy.axios.get(
+    //   `${proxy.envURL}/stockApi/sheetData/tech/macd`
+    // );
 
-        const { proxy } = getCurrentInstance()
+    // 設定template資料
+    let buy_page_list = ref([]);
+    let sell_page_list = ref([]);
 
-        function get() {
-          proxy.axios({
-            method: 'get',
-            url: `${proxy.envURL}/stockApi/investDB/local_listed`
-          }).then(res=>{
-            console.log(res);
-            // let { data } = res.data
-            // console.log(data)
-          }).catch((err) => {
-            console.log(err)
+    const get = async function () {
+      try {
+        // 技術分析的api請求
+        const [kd_condition] = await Promise.all([kd_promise])
+          .then((values) => {
+            return values;
+          })
+          .catch((err) => {
+            throw err;
           });
-        }
-
-
-        function post() {
-          proxy.axios({
-            method: 'post',
-            url: `${proxy.envURL}/word`,
-            data: {
-              firstName: 'Finn',
-              lastName: 'Williams'
-            }
-          }).then(res=>{
-            console.log(res);
-            // let { data } = res.data
-            // console.log(data)
-          }).catch((err) => {
-            console.log(err)
-          });
-        }
-        return {count, get, post}
+        // 法人一日買賣超的api請求
+        const { data } = await getInvestLocal;
+        const { sheetsData } = data;
+        buy_page_list.value = composeBaseBuySell(
+          sheetsData.local_listed_buy_no,
+          sheetsData.local_listed_buy_code,
+          sheetsData.local_listed_buy_name
+        );
+        sell_page_list.value = composeBaseBuySell(
+          sheetsData.local_listed_sell_no,
+          sheetsData.local_listed_sell_code,
+          sheetsData.local_listed_sell_name
+        );
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    /**
+     * @description 組成買進 or 賣出的陣列
+     * @param {*} no 名次
+     * @param {*} code 代號
+     * @param {*} name 名稱
+     */
+    function composeBaseBuySell(no, code, name) {
+      const result = [];
+      for (let i = 0, len = name.length; i < len; i++) {
+        result.push({
+          no: no[i],
+          code: code[i],
+          name: name[i],
+          tech: [],
+        });
+      }
+      return result;
     }
-}
+    function post() {
+      proxy
+        .axios({
+          method: "post",
+          url: `${proxy.envURL}/word`,
+          data: {
+            firstName: "Finn",
+            lastName: "Williams",
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          // let { data } = res.data
+          // console.log(data)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    return { get, post, buy_page_list, sell_page_list };
+  },
+};
 </script>
 <style>
 @media (min-width: 1024px) {

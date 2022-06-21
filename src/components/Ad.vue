89f3cac1481cbaf1ac1a11b5edf1fdaf6a00175f
liugoldent <liugoldent@gmail.com>
2022-06-21 21:23:01 +0800

    use line

--- src/components/Ad.vue
+++ src/components/Ad.vue
@@ -1,32 +1,37 @@
 <template>
   <div
-    class="h-auto w-60 flex flex-col justify-center items-end"
+    class="h-auto w-auto flex flex-col justify-center items-end"
     :class="{ invisible: visiableStatus }"
   >
     <font-awesome-icon
-      icon="xmark"
-      class="border-none text-4xl text-red-700 cursor-pointer"
+      :icon="['fas', 'xmark']"
+      class="border-none text-4xl text-red-500 cursor-pointer relative top-5"
       @click="visiableStatus = !visiableStatus"
     />
     <div
       v-for="product in productList"
       :key="product.name"
-      class="
-        rounded-lg
-        flex flex-col
-        justify-center
-        items-center
-      "
+      class="rounded-lg flex flex-col justify-center items-center"
     >
-      <p class="text-xl bg-red-400 w-full text-center">{{ product.name }}</p>
-      <a :href="product.href"><img :src="product.src" class="animate-pulse hover:animate-none"/></a>
+      <a
+        href="
+https://line.me/ti/g2/V1O2JN2etzTffgjw5NIq5-JvXF5FToNydOV5bw?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
+        ><font-awesome-icon
+          :icon="['fab', 'line']"
+          class="border-none text-8xl text-green-700 cursor-pointer"
+      /></a>
+      <!-- <p class="text-xl bg-red-400 w-full text-center">{{ product.name }}</p> -->
+      <!-- <a :href="product.href"
+        ><img :src="product.src" class="animate-pulse hover:animate-none"
+      /></a> -->
     </div>
   </div>
 </template>
 <script>
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
-library.add(faXmark)
+import { faLine } from '@fortawesome/fontawesome-free-brands'
+library.add(faXmark, faLine)
 
 import { ref } from 'vue'
 import { productList } from '../public/AdInfo.js'

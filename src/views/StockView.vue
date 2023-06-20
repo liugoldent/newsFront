c4c6ce39212448544b92762afeb36a80f811935d
keedem <liugoldent@gmail.com>
2023-06-20 18:08:31 +0800

    feat: 修改版面

--- src/views/StockView.vue
+++ src/views/StockView.vue
@@ -2,18 +2,14 @@
   <main class="flex flex-col justify-start items-center">
     <!-- 左右方button -->
     <div class="flex flex-row justify-between w-full fixed z-10 m-10">
-      <button
-        @click="toggleInvestStatus = !toggleInvestStatus"
+      <button @click="toggleInvestStatus = !toggleInvestStatus"
         class="fixed left-0 px-6 py-2 text-sm transition-colors rounded shadow-xl bg-slate-500 hover:bg-slate-600 text-slate-100 shadow-slate-400 duration-500"
-        :class="{ '-translate-x-24': toggleInvestStatus }"
-      >
+        :class="{ '-translate-x-24': toggleInvestStatus }">
         法人買賣超<font-awesome-icon icon="list" class="mx-2" />
       </button>
-      <button
-        @click="toggleTechStatus = !toggleTechStatus"
+      <button @click="toggleTechStatus = !toggleTechStatus"
         class="fixed right-0 px-6 py-2 text-sm transition-colors rounded shadow-xl bg-slate-500 hover:bg-slate-600 text-slate-100 shadow-slate-400 duration-500"
-        :class="{ 'translate-x-24': toggleTechStatus }"
-      >
+        :class="{ 'translate-x-24': toggleTechStatus }">
         <font-awesome-icon icon="list" class="mx-2" />
         技術分析&ensp;&thinsp;
       </button>
@@ -21,58 +17,61 @@
     <!-- 左方選單List -->
     <div
       class="flex flex-col justify-center items-start bg-slate-300 opacity-90 fixed left-0 z-10 my-20 p-4 rounded-md duration-500"
-      :class="{ '-translate-x-full': toggleInvestStatus }"
-    >
+      :class="{ '-translate-x-full': toggleInvestStatus }">
       <p class="border-l-2 border-b-2 rounded-b-md border-red-500 p-2 mb-1">
         資料更新時間：06/19
       </p>
-      <button
-        @click="getInvestCombineData(content)"
-        class="rounded-md border border-gray-500 p-2 mb-1"
-        v-for="content in buttonContent"
-        :key="content.key"
-      >
-        <font-awesome-icon
-          icon="arrow-trend-up"
-          style="color: red"
-          v-if="thisTimeClickType === content.key"
-          class="mr-2"
-        />
-        <title
-          class="inline"
-          :class="{
-            'underline underline-offset-2 text-red-600':
-              selectedButton === content.key,
-          }"
-        >
-          {{ content.name }}
-        </title>
+      <p class="border-l-2 border-b-2 rounded-b-md border-red-500 p-2 mb-1">
+        法人
+      </p>
+      <div class="flex">
+        <template v-for="(item, index) in investorType" :key="index">
+          <input type="checkbox" :id="`${item.type}${index}`" class="peer hidden" v-model="investorSelectedType"
+            :value="item.name" />
+          <label :for="`${item.type}${index}`" class="select-none cursor-pointer rounded-lg border-2 border-gray-200
+        py-3 px-6 font-bold text-gray-200 transition-colors duration-200 ease-in-out"
+            :class="{ 'peer-checked:bg-gray-200 peer-checked:text-gray-900 peer-checked:border-gray-200': investorSelectedType.includes(item.name) }">
+            {{ item.name }} </label>
+        </template>
+      </div>
+      <p class="border-l-2 border-b-2 rounded-b-md border-red-500 p-2 mb-1">
+        市場類別
+      </p>
+      <div class="flex">
+        <template v-for="(item, index) in marketType" :key="index">
+          <input type="checkbox" :id="`${item.type}${index}`" class="peer hidden" v-model="marketSelectedType"
+            :value="item.type" />
+          <label :for="`${item.type}${index}`" class="select-none cursor-pointer rounded-lg border-2 border-gray-200
+        py-3 px-6 font-bold text-gray-200 transition-colors duration-200 ease-in-out"
+            :class="{ 'peer-checked:bg-gray-200 peer-checked:text-gray-900 peer-checked:border-gray-200': marketSelectedType.includes(item.type) }">
+            {{ item.name }} </label>
+        </template>
+      </div>
+      <p class="border-l-2 border-b-2 rounded-b-md border-red-500 p-2 mb-1">
+        累積日期
+      </p>
+      <div class="flex">
+        <template v-for="(item, index) in accumDayType" :key="index">
+          <input type="checkbox" :id="`${item.type}${index}`" class="peer hidden" v-model="accumDaySelectedType"
+            :value="item.type" />
+          <label :for="`${item.type}${index}`" class="select-none cursor-pointer rounded-lg border-2 border-gray-200
+        py-3 px-6 font-bold text-gray-200 transition-colors duration-200 ease-in-out"
+            :class="{ 'peer-checked:bg-gray-200 peer-checked:text-gray-900 peer-checked:border-gray-200': accumDaySelectedType.includes(item.type) }">
+            {{ item.name }} </label>
+        </template>
+      </div>
+      <button v-if="false" @click="getInvestCombineData(content)" class="rounded-md border border-gray-500 p-2 mb-1">
+        Search
       </button>
     </div>
     <!-- 主要內容table -->
-    <div
-      class="flex flex-row justify-center items-start w-full sm:flex-col sm:items-center mt-8 sm:mt-40"
-    >
-      <StockInvestDBTableVue
-        :investData="buy_page_list"
-        title="買超"
-        @openModal="troggleModalF"
-        class="sm:mt-2"
-      />
-      <StockInvestDBTableVue
-        :investData="sell_page_list"
-        title="賣超"
-        @openModal="troggleModalF"
-        class="sm:mt-2"
-      />
+    <div class="flex flex-row justify-center items-start w-full sm:flex-col sm:items-center mt-8 sm:mt-40">
+      <StockInvestDBTableVue :investData="buy_page_list" title="買超" @openModal="troggleModalF" class="sm:mt-2" />
+      <StockInvestDBTableVue :investData="sell_page_list" title="賣超" @openModal="troggleModalF" class="sm:mt-2" />
     </div>
     <!-- 跳窗 -->
     <div class="fixed top-50 z-30" v-if="modalStatus">
-      <StockModalLink
-        @closeModal="troggleModalF"
-        :propsToModal="propsToModal"
-        :linkWeb="linkWeb"
-      />
+      <StockModalLink @closeModal="troggleModalF" :propsToModal="propsToModal" :linkWeb="linkWeb" />
     </div>
     <!-- 方塊顯示點選到的法人長方形 -->
     <div class="sm:visible md:visible invisible">
@@ -80,18 +79,18 @@
     </div>
     <!-- 捲軸到頂端 -->
     <ScrollTopButton />
+    {{  title  }}
   </main>
   <BlackMirrorVue :mirrorStatus="modalStatus" />
   <loading-view :loadingStatus="loadingStatus" />
 </template>
 <script>
 import StockInvestDBTableVue from "../components/Stock/StockInvestDBTable.vue";
-import StockFilterListVue from "../components/Stock/StockFilterList.vue";
 import StockModalLink from "../components/Stock/StockModalLink.vue";
 import RectangleNameVue from "../components/RectangleName.vue";
 import ScrollTopButton from "../components/ScrollTopButton.vue";
 import BlackMirrorVue from "../components/BlackMirror.vue";
-import { ref, getCurrentInstance, onMounted } from "vue";
+import { ref, getCurrentInstance, onMounted, watch, computed } from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faArrowTrendUp, faList } from "@fortawesome/free-solid-svg-icons";
 
@@ -99,7 +98,6 @@ library.add(faArrowTrendUp, faList);
 export default {
   components: {
     StockInvestDBTableVue,
-    StockFilterListVue,
     StockModalLink,
     RectangleNameVue,
     ScrollTopButton,
@@ -109,6 +107,14 @@ export default {
     const { proxy } = getCurrentInstance()
     // 使用loading參數來讓畫面變暗
     let loadingStatus = ref(true);
+    let investorSelectedType = ref([])
+    let marketSelectedType = ref([])
+    let accumDaySelectedType = ref([])
+    const title = computed(() =>{
+      const a = investorSelectedType.value.concat()
+      console.log(a)
+      console.log(investorSelectedType.value.concat())
+    })
     onMounted(async () => {
       loadingStatus.value = false;
     });
@@ -118,7 +124,6 @@ export default {
      */
     let buy_page_list = ref([]);
     let sell_page_list = ref([]);
-    let selectedButton = ref("");
     let titleName = ref("");
     let titleHref = ref("");
     let thisTimeClickType = ref("");
@@ -135,30 +140,6 @@ export default {
       } catch (error) {
         console.error(error);
       }
-    };
-    /**
-     * @description 組成買進 or 賣出的陣列
-     * @param {*} no 名次
-     * @param {*} code 代號
-     * @param {*} name 名稱
-     */
-    function composeBaseBuySell(code, name) {
-      const result = [];
-      for (let i = 0, len = name.length; i < len; i++) {
-        result.push({
-          no: i,
-          code: code[i].replace("code-", ""),
-          name: name[i],
-        });
-      }
-      return result;
-    }
-    /**
-     * @description 接收 emit 資料 並將其塞入condition array
-     */
-    const conditionStatus = ref([]);
-    const updateFilterCondition = function (result) {
-      conditionStatus.value = result.value;
     };
     /**
      * @description 接收子組件上來的資料，並且開啟/關閉視窗
@@ -217,94 +198,60 @@ export default {
       },
     ];
     /**
-     * @description 上方button的資料
+     * @description 建立法人類別
      */
-    let buttonContent = ref([]);
-    buttonContent.value = [
+    let investorType = [
       {
-        name: "上市投信外資共同買賣超",
-        key: "lf_listed_1",
+        type: 'foreign',
+        name: '外資'
       },
       {
-        name: "上櫃投信外資共同買賣超",
-        key: "lf_otc_1",
+        type: 'local',
+        name: '投信'
       },
       {
-        name: "上市主力共同買賣超",
-        key: "local_listed",
+        type: 'employed',
+        name: '自營商'
       },
-    ];
+      {
+        type: 'main',
+        name: '主力'
+      },
+    ]
     /**
-     * 組成主力同買同賣資料
+     * @description 建立市場類別
      */
-    const composeSameArray = async function () {
-      const { data } = await investAllData;
-      const mainInvestObj = {
-        mainInvest_fl: ["foreign", "local"],
-        mainInvest_all: ["foreign", "local", "employed"],
-      };
-      const selectType = mainInvestObj[selectedButton.value];
-      const listedDataFunc = function () {
-        let list = [];
-        for (let i = 0, len = selectType.length; i < len; i++) {
-          list.push(data[`${selectType[i]}_listed`]);
-        }
-        return list;
-      };
-      const otcDataFunc = function () {
-        let list = [];
-        for (let i = 0, len = selectType.length; i < len; i++) {
-          list.push(data[`${selectType[i]}_otc`]);
-        }
-        return list;
-      };
-      const listedBuyRes = concatArr(listedDataFunc(), "buy");
-      const otcBuyRes = concatArr(otcDataFunc(), "buy");
-      const listedSellRes = concatArr(listedDataFunc(), "sell");
-      const otcSellRes = concatArr(otcDataFunc(), "sell");
-      buy_page_list.value = composeBaseBuySell(
-        listedBuyRes.name.concat(otcBuyRes.name).map((el, index) => index + 1),
-        listedBuyRes.code.concat(otcBuyRes.code),
-        listedBuyRes.name.concat(otcBuyRes.name)
-      );
-      sell_page_list.value = composeBaseBuySell(
-        listedSellRes.name
-          .concat(otcSellRes.name)
-          .map((el, index) => index + 1),
-        listedSellRes.code.concat(otcSellRes.code),
-        listedSellRes.name.concat(otcSellRes.name)
-      );
-    };
-    const concatArr = function (arrListed, type) {
-      try {
-        let name = [];
-        let code = [];
-        for (let i = 0, len = arrListed.length; i < len; i++) {
-          name.push(arrListed[i][`${arrListed[i].type}_${type}_name`]);
-          code.push(arrListed[i][`${arrListed[i].type}_${type}_code`]);
-        }
-        name = findRepeatEle(name);
-        code = findRepeatEle(code);
-
-        return {
-          name,
-          code,
-        };
-      } catch (error) {
-        console.log(error);
+    let marketType = [
+      {
+        type: 'listed',
+        name: '上市'
+      },
+      {
+        type: 'otc',
+        name: '上櫃'
       }
-    };
-    const findRepeatEle = function (arr) {
-      return arr.reduce(function (accumulator, curValue) {
-        if (accumulator.length === 0) {
-          accumulator = curValue;
-          return accumulator;
-        } else {
-          accumulator = accumulator.filter((ele) => curValue.includes(ele));
-          return accumulator;
-        }
-      }, []);
-    };
+    ]
+    /**
+     * @description 建立積累日子
+     */
+    let accumDayType = [
+      {
+        type: '1',
+        name: '1日'
+      },
+      {
+        type: '5',
+        name: '5日'
+      },
+      {
+        type: '10',
+        name: '10日'
+      },
+      {
+        type: '30',
+        name: '30日'
+      }
+    ]
     /**
      * @description 切換選單進入or移出
      */
@@ -312,24 +259,25 @@ export default {
     let toggleTechStatus = ref(window.innerWidth < 1200 ? true : false);
     return {
       getInvestCombineData, // 取得法人買賣超資料（打api
-      updateFilterCondition, // 更新使用者選到的指標function
       troggleModalF, // emit上來的function，目的是用於開啟關閉視窗
-      composeSameArray, // 用來測試api資料
       toggleInvestStatus, // 切換法人買賣超出現與否的參數
       toggleTechStatus, // 切換技術分析出現與否的參數
-      conditionStatus, // client選擇到的技術分析指標
       buy_page_list, // 法人買超資料
       sell_page_list, // 法人賣超資料
       modalStatus, // modal的開啟狀況
       propsToModal, // 給modal的資料
       linkWeb, // 給modal的連結資料
-      getInvestCombineData, // 上方 button的資料
       titleName, // 點完之後要顯示在畫面上的名稱
       titleHref, // 點完之後要顯示在畫面左上角的連結
       loadingStatus, //點進畫面的初始loading
-      selectedButton, // 選到的法人買賣
-      buttonContent, // 左邊法人按鈕形式
       thisTimeClickType, // 此次點到的法人種類
+      investorType, // 所有法人種類
+      investorSelectedType, // 選到的法人種類
+      marketType, // 市場類別
+      marketSelectedType,
+      accumDayType, // 積累日期類別
+      accumDaySelectedType,
+      title
     };
   },
 };
